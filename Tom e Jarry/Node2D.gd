f66d0c5cbe71251d5bedb0715f0154e72046bf26
Rodrigo Sales <freirerodrigosales@gmail.com>
2023-02-09 20:08:35 -0300

    Pausar animação de explosão

--- Tom e Jarry/Node2D.gd
+++ Tom e Jarry/Node2D.gd
@@ -31,7 +31,6 @@ func _process(delta):
 	var random_number_x_1 = rand_range(-10, 10)
 	var random_number_y_1 = rand_range(-10, 10) 
 
-
 	# adiciona um valor aleatório a posição atual dos objetos e define essa posição como posição atual
 	# clamp -> se o valor é menor que o mínimo, retorna o mínimo, se o valor é maior que o máximo, retorna o máximo. Caso contrário retorna o valor atual 
 	if (test_value1 == true):
@@ -46,12 +45,6 @@ func _process(delta):
 	else:
 		test_value = false
 
-#	print(random_number_x)
-#	print(button)
-	# teste das posição do objeto
-#	print($Sprite.position)
-#	print(random_number_x)
-
 	# Faz com que quando os objetos estão próximos um dos outros o comando é executado (o texto aparece)
 	if $tom.position.distance_to($jerry.position) < 100:
 		test_value = false
@@ -60,4 +53,6 @@ func _process(delta):
 		$Sprite.position.x = $tom.position.x
 		$Sprite.position.y = $tom.position.y
 		$Sprite/AnimationPlayer.play("explosão")
+		yield(get_tree().create_timer(7), "timeout") # pausa uma função, executa um comando até o fim dele e resume a função novamente
+		$Sprite/AnimationPlayer.stop()
 		
